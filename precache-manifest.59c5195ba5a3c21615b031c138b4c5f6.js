1d322518068a11f8a9e903718c930075b211389b
Kalina Jakubowska <witillie@gmail.com>
2020-10-07 00:10:57 +0200

    Updates

---
+++ precache-manifest.59c5195ba5a3c21615b031c138b4c5f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17819af09656d08abb0e2dda304373a7",
+    "url": "/movie-browser/index.html"
+  },
+  {
+    "revision": "8fba57bac816341a5832",
+    "url": "/movie-browser/static/js/2.eaffff79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-browser/static/js/2.eaffff79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c988cd32bd99fbe9eb58",
+    "url": "/movie-browser/static/js/main.c8937979.chunk.js"
+  },
+  {
+    "revision": "940b2a428110daf5a21e",
+    "url": "/movie-browser/static/js/runtime-main.6da0a001.js"
+  }
+]);
