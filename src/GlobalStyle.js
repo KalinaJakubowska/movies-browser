b162916b6b70b78e645132e43cfef2a4932c8640
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2020-10-07 00:31:56 +0200

    little changes in global styles

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -4,9 +4,16 @@ export const GlobalStyle = createGlobalStyle`
    html {
       box-sizing: border-box;
    }
+
    *,
    ::before,
    ::after {
       box-sizing: inherit;
    }
+
+   body{
+      background: ${({theme}) => theme.colors.Mercury};
+   }
+
+  
 `;
