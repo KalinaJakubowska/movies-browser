5a7dfd1ba89210fd92cc03d71c56bded9effa407
Kalina Jakubowska <witillie@gmail.com>
2020-10-07 15:30:26 +0200

    store added

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import {GlobalStyle} from "./GlobalStyle";
-import {ThemeProvider} from "styled-components";
-import {theme} from "./theme";
+import { GlobalStyle } from "./GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root"),
 );
