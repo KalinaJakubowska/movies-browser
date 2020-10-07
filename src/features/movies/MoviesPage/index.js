163df42c5d004919be63c267738509b21b8a0201
Kalina Jakubowska <witillie@gmail.com>
2020-10-07 16:22:26 +0200

    very simple displaying added

--- src/features/movies/MoviesPage/index.js
+++ src/features/movies/MoviesPage/index.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    fetchPopularMovies,
+    selectPopularMovies,
+    selectLoading
+} from "./moviesSlice";
 
 const MoviesPage = () => {
+    const popularMovies = useSelector(selectPopularMovies);
+    const isLoading = useSelector(selectLoading);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchPopularMovies());
+    }, [])
 
     return (
-        <p>Movies page</p>
+        <div>{isLoading
+            ? "loading"
+            : popularMovies.map(movie => <p key={movie.id}>{movie.title}</p>)
+        }</div>
     );
 };
 
