c70adc274103212bea09653154ebbb6eb16b3ef1
Kalina Jakubowska <witillie@gmail.com>
2020-10-08 14:08:44 +0200

    MovieTile styled

--- src/features/MovieTile/index.js
+++ src/features/MovieTile/index.js
@@ -1,20 +1,28 @@
 import React from "react";
-import { Tile } from "./styled";
+import {
+    Tile,
+    Image,
+    Title,
+    Year,
+    DetailsContainer,
+    Container
+} from "./styled";
 
 const MovieTile = ({ movieInfo }) => {
     console.log(movieInfo)
     return (
         <Tile>
-            <div >
-            </div>
-            <div>
-                <p>{movieInfo.title}
-                </p>
-                <p>{movieInfo.release_date}
-                </p>
-                <p ></p>
-                <p ></p>
-            </div>
+            <Image src={movieInfo.poster_path} alt={`Plakat filmu ${movieInfo.title}`} />
+            <DetailsContainer>
+                <Container>
+                    <Title>{movieInfo.title}
+                    </Title>
+                    <Year>{(movieInfo.release_date).slice(0, 4)}
+                    </Year>
+                    <div >Types</div>
+                </Container>
+                <div >Ratings</div>
+            </DetailsContainer>
         </Tile>
     );
 }
