601921b0a2ed234dc9a7700f3946e6d0d9b5ca95
Kalina Jakubowska <witillie@gmail.com>
2020-10-08 19:44:32 +0200

    BottomNavbar styled

--- src/features/BottomNavbar/styled.js
+++ src/features/BottomNavbar/styled.js
@@ -1,5 +1,35 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+width: fit-content;
+    margin: 40px auto 100px;
+`
+
 export const Button = styled.button`
     padding: 10px;
+    margin: 0px 12px;
+    background: #D6E4FF;
+    border-radius: 5px;
+    padding: 8px 16px 8px 16px;
+    color:${({ theme }) => theme.colors.mineShaft};
+    border: none;
+    cursor: pointer;
+
+    &:disabled {
+        background-color: ${({ theme }) => theme.colors.cornsilk};
+    }
+`
+
+export const PageCounter = styled.span`
+    font-size: 16px;
+    line-height: 150%;
+    color: ${({ theme }) => theme.colors.waterloo};
+    margin: 0px 24px;
+`
+
+export const Number = styled.span`
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 150%;
+    color: ${({ theme }) => theme.colors.woodsmoke};
 `
