ff4e2322abdb3a1a9cc30faf87a998106ce51f66
Kalina Jakubowska <witillie@gmail.com>
2020-10-08 19:13:43 +0200

    max page number set dinamically

--- src/features/BottomNavbar/index.js
+++ src/features/BottomNavbar/index.js
@@ -1,18 +1,42 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectActivePage, setActivePage } from "../movies/MoviesPage/moviesSlice";
-import { Button } from "./styled";
+import { selectActivePage, selectNumberPages, setActivePage } from "../movies/MoviesPage/moviesSlice";
+import { Button, PageCounter, Number } from "./styled";
 
 const BottomNavbar = () => {
     const activePage = useSelector(selectActivePage);
+    const numberPages = useSelector(selectNumberPages);
     const dispatch = useDispatch();
 
     return (
         <div>
-            <Button disabled={activePage === 1} onClick={() => dispatch(setActivePage(1))}>{"<<"}</Button>
-            <Button disabled={activePage === 1} onClick={() => dispatch(setActivePage(activePage - 1))}>{"<"}</Button>
-            <Button disabled={activePage === 500} onClick={() => dispatch(setActivePage(activePage + 1))}>{">"}</Button>
-            <Button disabled={activePage === 500} onClick={() => dispatch(setActivePage(500))}>{">>"}</Button>
+            <Button
+                disabled={activePage === 1} onClick={() =>
+                    dispatch(setActivePage(1))}
+            >
+                {"< First"}
+            </Button>
+            <Button
+                disabled={activePage === 1} onClick={() =>
+                    dispatch(setActivePage(activePage - 1))}
+            >
+                {"< Previous"}
+            </Button>
+            <PageCounter>
+                Page <Number>{activePage}</Number> of <Number>{numberPages}</Number>
+            </PageCounter>
+            <Button
+                disabled={activePage === numberPages} onClick={() =>
+                    dispatch(setActivePage(activePage + 1))}
+            >
+                {"Next >"}
+            </Button>
+            <Button
+                disabled={activePage === numberPages} onClick={() =>
+                    dispatch(setActivePage(numberPages))}
+            >
+                {"Last >"}
+            </Button>
         </div>
     );
 };
