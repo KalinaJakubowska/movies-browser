ef81409a457c84bba472819e73d7bc7e57882e15
Kalina Jakubowska <witillie@gmail.com>
2020-10-08 19:44:15 +0200

    color mercury changed to whisper

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,12 @@
 export const theme = {
   colors: {
-    mercury: "#E5E5E5",
+    whisper: "#F5F5FA",
     black: "#000000",
     waterloo: "#7E839A",
     tan: "#F5F5FA",
     cornsilk: "#E4E6F0",
     white: "#FFFFFF",
     woodsmoke: "#18181B",
+    mineShaft: "#333333",
   },
 };
