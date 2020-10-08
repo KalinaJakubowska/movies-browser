e49949d28090f526cc6c38bd74ab19f50ba6e973
Kalina Jakubowska <witillie@gmail.com>
2020-10-08 14:08:16 +0200

    MoviesContainer styled

--- src/features/movies/MoviesPage/styled.js
+++ src/features/movies/MoviesPage/styled.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 export const MoviesContainer = styled.div`
-display: grid;
-grid-template-columns: 1fr 1fr 1fr 1fr;
-grid-gap: 24px;
+    width: 1368px;
+    margin: auto;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 24px;
 `
