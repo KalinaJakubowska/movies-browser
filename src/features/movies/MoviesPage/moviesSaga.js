02c6e131007d4020be6c953c62efd7ba14b0ec8e
Kalina Jakubowska <witillie@gmail.com>
2020-10-08 19:11:29 +0200

    all results handed over to fetchPopularMoviesSuccess

--- src/features/movies/MoviesPage/moviesSaga.js
+++ src/features/movies/MoviesPage/moviesSaga.js
@@ -10,7 +10,7 @@ function* fetchExampleTasksHandler() {
     try {
         yield delay(500);
         const popularMovies = yield call(getPopularMovies);
-        yield put(fetchPopularMoviesSuccess(popularMovies.results));
+        yield put(fetchPopularMoviesSuccess(popularMovies));
     } catch (error) {
         yield put(fetchPopularMoviesError());
         yield call(alert, "Coś poszło nie tak");
