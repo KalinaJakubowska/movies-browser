a471974bf4cb1e379b75250aaf2a93b17b66ca6a
Kalina Jakubowska <witillie@gmail.com>
2020-10-09 01:19:08 +0200

    pageParameter custom Hooks created to save and get page number  from URL

---
+++ src/features/pageParameters.js
@@ -0,0 +1,25 @@
+import { useHistory, useLocation } from "react-router-dom";
+
+export const usePageParameter = paramName => {
+    const location = useLocation();
+
+    return (new URLSearchParams(location.search)).get(paramName);
+};
+
+export const useReplacePageParameter = () => {
+    const history = useHistory();
+    const location = useLocation();
+
+    const searchParams = new URLSearchParams(location.search);
+
+    const replacePageParameter = ( key, value ) => {
+        if (!value === "") {
+            searchParams.delete(key);
+        } else {
+            searchParams.set(key, value);
+        }
+        history.replace(`${location.pathname}?${searchParams.toString()}`);
+    };
+
+    return replacePageParameter;
+};
