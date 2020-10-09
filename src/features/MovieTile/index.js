2fd2819da66482ea799a4ce0a2cccde98751dbc7
Kalina Jakubowska <witillie@gmail.com>
2020-10-09 21:47:11 +0200

    display conditions added

--- src/features/MovieTile/index.js
+++ src/features/MovieTile/index.js
@@ -7,22 +7,33 @@ import {
     DetailsContainer,
     Container
 } from "./styled";
+import noPosterImage from "./../../assets/noPosterImage.svg"
 
 const MovieTile = ({ movieInfo }) => {
     return (
         <Tile>
-            <Image src={`https://image.tmdb.org/t/p/w342${movieInfo.poster_path}`} alt={`Plakat filmu ${movieInfo.title}`} />
+            <Image
+                src={movieInfo.poster_path
+                    ? `https://image.tmdb.org/t/p/w342${movieInfo.poster_path}`
+                    : noPosterImage
+                }
+                alt={`Plakat filmu ${movieInfo.title}`}
+            />
             <DetailsContainer>
                 <Container>
-                    <Title>{movieInfo.title}
-                    </Title>
-                    <Year>{(movieInfo.release_date).slice(0, 4)}
-                    </Year>
+                    {movieInfo.title &&
+                        <Title> {movieInfo.title}
+                        </Title>
+                    }
+                    {movieInfo.release_date &&
+                        <Year> {(movieInfo.release_date).slice(0, 4)}
+                        </Year>
+                    }
                     <div >Types</div>
                 </Container>
                 <div >Ratings</div>
             </DetailsContainer>
-        </Tile>
+        </Tile >
     );
 }
 
