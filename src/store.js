6f80c5c0cb8942d5e99b9c165f737cf9adc5eb67
Kalina Jakubowska <witillie@gmail.com>
2020-10-12 00:54:28 +0200

    rootSaga added

--- src/store.js
+++ src/store.js
@@ -1,17 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from "./features/movies/MoviesPage/moviesSlice";
+import movieReducer from "./features/movies/MoviePage/movieSlice";
 import createSagaMiddleware from "redux-saga";
-import { watchSetActivePage } from "./features/movies/MoviesPage/moviesSaga"
+import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         movies: moviesReducer,
+        movie: movieReducer,
     },
     middleware: [sagaMiddleware],
 });
 
-sagaMiddleware.run(watchSetActivePage);
+sagaMiddleware.run(rootSaga);
 
 export default store;
