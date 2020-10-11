9824f31e36d927e0b9f6d6bb348d43027ebabe53
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2020-10-12 01:33:33 +0200

    responsive tile grid

--- src/features/movies/MoviesPage/styled.js
+++ src/features/movies/MoviesPage/styled.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 export const MoviesContainer = styled.div`
-    width: 1368px;
-    margin: 24px auto 40px;
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-gap: 24px;
-`
+  max-width: 1368px;
+  margin: 24px auto 40px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 320px);
+  grid-gap: 24px;
+  justify-content: center;
+`;
