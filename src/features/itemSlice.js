92b5cd41014b00ebc1da2baa43da599fb03f3c4b
Kalina Jakubowska <witillie@gmail.com>
2020-10-12 22:21:50 +0200

    second path and extra data added to state

--- src/features/itemSlice.js
+++ src/features/itemSlice.js
@@ -1,33 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
-import listPaths from "../common/listPaths";
 
 const itemSlice = createSlice({
     name: "item",
     initialState: {
         itemData: [],
+        extraData: [],
         loading: true,
         itemId: undefined,
-        activePath: "",
+        activeItemPath: "",
+        activeExtraPath: "",
     },
     reducers: {
         setItemId: (state, { payload: id }) => {
             state.loading = true;
             state.itemId = id;
         },
-        fetchItemSuccess: (state, { payload: itemData }) => {
+        fetchItemSuccess: (state, { payload: itemData, extraData }) => {
             state.itemData = itemData;
+            state.extraData = extraData;
             state.loading = false;
         },
         fetchItemError: state => {
             state.loading = false;
         },
-        setActivePath: (state, {payload: name}) => {
-            state.activePath = listPaths[name];
+        setActivePath: (state, { payload: { path1, path2 } }) => {
+            state.activeItemPath = path1;
+            state.activeExtraPath = path2;
         },
         resetState: (state) => {
             state.itemData = [];
+            state.extraData = [];
             state.itemId = undefined;
-            state.activePath = "";
+            state.activeItemPath = "";
+            state.activeExtraPath = "";
             state.loading = true;
         },
     },
@@ -41,6 +46,7 @@ export const {
     resetState,
 } = itemSlice.actions;
 export const selectItemData = state => state.item.itemData;
+export const selectExtraData = state => state.item.extraData;
 export const selectLoading = state => state.item.loading;
 
 export default itemSlice.reducer;
