f7834569e543c21ee349a1f5b262bb747d10bf17
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2020-10-13 01:59:35 +0200

    media queries for mobile

--- src/common/Ratings/styled.js
+++ src/common/Ratings/styled.js
@@ -9,15 +9,29 @@ export const Container = styled.div`
 export const Star = styled.img`
   width: 24px;
   height: 22.87px;
+
+  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+    width: 16px;
+    height: 15.25px;
+  }
 `;
 
 export const Rate = styled.span`
   font-size: 16px;
   font-weight: 600;
   margin: 0 12px;
+
+  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+    font-size: 13px;
+    margin: 0 7px;
+  }
 `;
 
 export const Votes = styled.span`
   font-size: 16px;
-  color: ${({ theme }) => theme.colors.waterloo};
+  color: ${({theme}) => theme.colors.waterloo};
+
+  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+    font-size: 13px;
+  }
 `;
