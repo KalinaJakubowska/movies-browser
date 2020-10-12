209d82ff91d1d86eb845d234cbc06e4d0273a3e4
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2020-10-13 01:17:52 +0200

    after-merge fixes

--- src/common/tiles/TileContainer.js
+++ src/common/tiles/TileContainer.js
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 
-export const MoviesContainer = styled.div`
+export const MovieContainer = styled.div`
   max-width: 1368px;
   margin: 24px auto 40px;
   display: grid;
   grid-template-columns: repeat(auto-fill, 320px);
   grid-gap: 24px;
   justify-content: center;
+`;
+
+export const PeopleContainer = styled(MovieContainer)`
+  grid-template-columns: repeat(6, 1fr);
 `;
--- src/features/movies/MoviePage/index.js
+++ src/features/movies/MoviePage/index.js
@@ -1,69 +1,75 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {useParams} from "react-router-dom";
 import Loading from "../../../common/Loading";
 import {
-    setActivePath,
-    selectLoading,
-    selectItemData,
-    setItemId,
-    selectExtraData
+  setActivePath,
+  selectLoading,
+  selectItemData,
+  setItemId,
+  selectExtraData,
 } from "../../itemSlice";
 import language from "./../../../common/language";
 import apiKey from "./../../../common/apiKey";
-import { PeopleContainer } from "../../../common/tiles/TileContainer";
+import {PeopleContainer} from "../../../common/tiles/TileContainer";
 import PersonTile from "../../../common/tiles/PersonTile";
 import Header from "../../../common/Header";
 
 const MoviePage = () => {
-    const { id } = useParams();
-    const dispatch = useDispatch()
-    const movieData = useSelector(selectItemData);
-    const castCrewData = useSelector(selectExtraData);
-    const loading = useSelector(selectLoading);
+  const {id} = useParams();
+  const dispatch = useDispatch();
+  const movieData = useSelector(selectItemData);
+  const castCrewData = useSelector(selectExtraData);
+  const loading = useSelector(selectLoading);
 
-    useEffect(() => {
-        dispatch(setActivePath({
-            path1: `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=${language}`,
-            path2: `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}`,
-        }));
-        dispatch(setItemId(id));
-    }, [id])
-
-    return (
-        <>
-            {loading
-                ? <Loading />
-                :
-                <>
-                    <Header as="h2">Cast</Header>
-                    <PeopleContainer>
-                        {castCrewData.cast.slice(0,10).map(({ profile_path, id, character, name }) =>
-                            <PersonTile
-                                key={id}
-                                profile_path={profile_path}
-                                id={id}
-                                name={name}
-                                subtitle={character}
-                            />
-                        )}
-                    </PeopleContainer>
-                    <Header as="h2">Crew</Header>
-                    <PeopleContainer>
-                        {castCrewData.crew.slice(0,10).map(({ profile_path, id, job, name, credit_id }) =>
-                            <PersonTile
-                                key={credit_id}
-                                profile_path={profile_path}
-                                id={id}
-                                name={name}
-                                subtitle={job}
-                            />
-                        )}
-                    </PeopleContainer>
-                </>
-            }
-        </>
+  useEffect(() => {
+    dispatch(
+      setActivePath({
+        path1: `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=${language}`,
+        path2: `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}`,
+      }),
     );
+    dispatch(setItemId(id));
+  }, [id]);
+
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Header as="h2">Cast</Header>
+          <PeopleContainer>
+            {castCrewData.cast
+              .slice(0, 10)
+              .map(({profile_path, id, character, name}) => (
+                <PersonTile
+                  key={id}
+                  profile_path={profile_path}
+                  id={id}
+                  name={name}
+                  subtitle={character}
+                />
+              ))}
+          </PeopleContainer>
+          <Header as="h2">Crew</Header>
+          <PeopleContainer>
+            {castCrewData.crew
+              .slice(0, 10)
+              .map(({profile_path, id, job, name, credit_id}) => (
+                <PersonTile
+                  key={credit_id}
+                  profile_path={profile_path}
+                  id={id}
+                  name={name}
+                  subtitle={job}
+                />
+              ))}
+          </PeopleContainer>
+        </>
+      )}
+    </>
+  );
 };
 
 export default MoviePage;
