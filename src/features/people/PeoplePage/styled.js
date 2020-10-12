5da15479b1a00f7ce6f8dcb5b3e90d844aebf6be
Kalina Jakubowska <witillie@gmail.com>
2020-10-12 16:18:59 +0200

    PeopleContainer added

---
+++ src/features/people/PeoplePage/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const PeopleContainer = styled.div`
+    width: 1368px;
+    margin: 24px auto 40px;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-gap: 24px;
+`
