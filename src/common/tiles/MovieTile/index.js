bb26982f458651049a7671d5d8811d6543f0f1b4
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2020-10-13 01:59:14 +0200

    added MovieWrapper component

--- src/common/tiles/MovieTile/index.js
+++ src/common/tiles/MovieTile/index.js
@@ -1,44 +1,41 @@
 import React from "react";
 import Ratings from "./../../Ratings";
 import {
-    Wrapper,
-    Image,
-    Title,
-    Year,
-    Container,
-    DetailsContainer,
+  MovieWrapper,
+  Image,
+  Title,
+  Year,
+  Container,
+  DetailsContainer,
 } from "./../tileStyles";
-import noPosterImage from "./../../../assets/noPosterImage.svg"
+import noPosterImage from "./../../../assets/noPosterImage.svg";
 
-const MovieTile = ({ movieInfo }) => {
-    return (
-        <Wrapper to={`/movies/movie/${movieInfo.id}`}>
-            <Image
-                src={movieInfo.poster_path
-                    ? `https://image.tmdb.org/t/p/w342${movieInfo.poster_path}`
-                    : noPosterImage
-                }
-                alt={`Plakat filmu ${movieInfo.title}`}
-            />
-            <DetailsContainer>
-                <Container>
-                    {movieInfo.title &&
-                        <Title> {movieInfo.title}
-                        </Title>
-                    }
-                    {movieInfo.release_date &&
-                        <Year> {(movieInfo.release_date).slice(0, 4)}
-                        </Year>
-                    }
-                    <div >Types</div>
-                </Container>
-                <Ratings
-                    voteAverage={movieInfo.vote_average}
-                    voteCount={movieInfo.vote_count}
-                />
-            </DetailsContainer>
-        </Wrapper >
-    );
+const MovieTile = ({movieInfo}) => {
+  return (
+    <MovieWrapper to={`/movies/movie/${movieInfo.id}`}>
+      <Image
+        src={
+          movieInfo.poster_path
+            ? `https://image.tmdb.org/t/p/w342${movieInfo.poster_path}`
+            : noPosterImage
+        }
+        alt={`Plakat filmu ${movieInfo.title}`}
+      />
+      <DetailsContainer>
+        <Container>
+          {movieInfo.title && <Title> {movieInfo.title}</Title>}
+          {movieInfo.release_date && (
+            <Year> {movieInfo.release_date.slice(0, 4)}</Year>
+          )}
+          <div>Types</div>
+        </Container>
+        <Ratings
+          voteAverage={movieInfo.vote_average}
+          voteCount={movieInfo.vote_count}
+        />
+      </DetailsContainer>
+    </MovieWrapper>
+  );
 };
 
 export default MovieTile;
--- src/common/tiles/tileStyles.js
+++ src/common/tiles/tileStyles.js
@@ -1,48 +1,74 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import styled from "styled-components";
 
 export const Wrapper = styled(Link)`
-    text-decoration: none;
-    display: flex;
-    flex-direction: column;
-    width:324px;
-    height:650px;
-    padding: 16px;
-    background-color: ${({ theme }) => theme.colors.white};
-    box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
-    border-radius: 5px;
-    color: ${({ theme }) => theme.colors.woodsmoke};
-`
+  text-decoration: none;
+  display: flex;
+  flex-direction: column;
+  width: 324px;
+  height: 650px;
+  padding: 16px;
+  background-color: ${({theme}) => theme.colors.white};
+  box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
+  border-radius: 5px;
+  color: ${({theme}) => theme.colors.woodsmoke};
+`;
+
+export const MovieWrapper = styled(Wrapper)`
+  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+    flex-direction: row;
+    width: 100%;
+    height: 201px;
+  }
+`;
 
 export const Image = styled.img`
-    display: block;
-    height: 434px;
-    width: 292px;
-    border-radius: 5px;
-    margin-bottom: 16px;
-`
+  display: block;
+  height: 434px;
+  width: 292px;
+  border-radius: 5px;
+  margin-bottom: 16px;
+
+  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+    width: 114px;
+    height: 169px;
+  }
+`;
 
 export const Container = styled.div`
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-gap: 8px;
-`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 8px;
+`;
 
 export const Title = styled.div`
-    font-weight: 500;
-    font-size: 22px;
-    line-height: 130%;
-`
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 130%;
+
+  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+    font-size: 16px;
+  }
+`;
 
 export const Year = styled.div`
-    font-size: 16px;
-    line-height: 150%;
-    color: ${({ theme }) => theme.colors.Waterloo};
-`
+  font-size: 16px;
+  line-height: 150%;
+  color: ${({theme}) => theme.colors.waterloo};
+
+  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+    font-size: 13px;
+  }
+`;
 
 export const DetailsContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    flex-grow: 1;
-    justify-content: space-between;
-`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  justify-content: space-between;
+
+  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+    margin-left: 16px;
+    justify-content: unset;
+  }
+`;
