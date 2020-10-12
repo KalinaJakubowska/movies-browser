64eda88826f029ee6fb8c8868ab32cfb18a4c367
Kalina Jakubowska <witillie@gmail.com>
2020-10-12 22:22:46 +0200

    castCrewData fetching added

--- src/features/movies/MoviePage/index.js
+++ src/features/movies/MoviePage/index.js
@@ -2,16 +2,28 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import Loading from "../../../common/Loading";
-import { setActivePath, selectLoading, selectItemData, setItemId } from "../../itemSlice";
+import {
+    setActivePath,
+    selectLoading,
+    selectItemData,
+    setItemId,
+    selectExtraData
+} from "../../itemSlice";
+import language from "./../../../common/language";
+import apiKey from "./../../../common/apiKey";
 
 const MoviePage = () => {
     const { id } = useParams();
     const dispatch = useDispatch()
     const movieData = useSelector(selectItemData);
+    const castCrewData = useSelector(selectExtraData);
     const loading = useSelector(selectLoading);
 
     useEffect(() => {
-        dispatch(setActivePath("movie"));
+        dispatch(setActivePath({
+            path1: `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=${language}`,
+            path2: `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}`,
+        }));
         dispatch(setItemId(id));
     }, [id])
 
