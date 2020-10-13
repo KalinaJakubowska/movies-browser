008a0a91c98a01e23e47c78d12bdb39ea12e6699
Kalina Jakubowska <witillie@gmail.com>
2020-10-13 19:41:48 +0200

    MovieContainer grid items width changed to 324px

--- src/common/tiles/TileContainer.js
+++ src/common/tiles/TileContainer.js
@@ -4,9 +4,10 @@ export const MovieContainer = styled.div`
   max-width: 1368px;
   margin: 24px auto 40px;
   display: grid;
-  grid-template-columns: repeat(auto-fill, 320px);
+  grid-template-columns: repeat(auto-fill, 324px);
   grid-gap: 24px;
   justify-content: center;
+
   @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
     width: 100%;
     grid-gap: 16px;
