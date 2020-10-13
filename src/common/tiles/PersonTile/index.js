2fc7b5f209e123e8535979d37c13bf9798b37763
Kalina Jakubowska <witillie@gmail.com>
2020-10-13 21:50:29 +0200

    Subtitle font-size changing when text too long added, PersonName font changed to 18px

--- src/common/tiles/PersonTile/index.js
+++ src/common/tiles/PersonTile/index.js
@@ -21,10 +21,14 @@ const PersonTile = ({ profile_path, id, name, subtitle = "" }) => {
             <PersonDetailsContainer>
                 <>
                     {name &&
-                        <PersonName>{name}</PersonName>
+                        <PersonName textLength={name.length}>
+                            {name}
+                        </PersonName>
                     }
                     {subtitle &&
-                        <Subtitle>{subtitle}</Subtitle>
+                        <Subtitle textLength={subtitle.length}>
+                            {subtitle}
+                        </Subtitle>
                     }
                 </>
             </PersonDetailsContainer>
