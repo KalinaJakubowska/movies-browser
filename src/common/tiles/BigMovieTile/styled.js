68a529e604a2132efaa73395dc78ea7410ae0d31
Kalina Jakubowska <witillie@gmail.com>
2020-10-14 21:03:56 +0200

    BigMovieTile created

---
+++ src/common/tiles/BigMovieTile/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { BigTileImage } from "../bigTilesStyles";
+
+export const BigMovieImage = styled(BigTileImage)`
+    width: 312px;
+    height: 464px;
+`;
