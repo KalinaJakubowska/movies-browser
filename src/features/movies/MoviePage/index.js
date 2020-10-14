ecd870bc441523109ab5296f85cb35d538284e74
Kalina Jakubowska <witillie@gmail.com>
2020-10-14 21:04:13 +0200

    BigMovieTile added

--- src/features/movies/MoviePage/index.js
+++ src/features/movies/MoviePage/index.js
@@ -14,6 +14,7 @@ import apiKey from "./../../../common/apiKey";
 import { PeopleContainer } from "../../../common/tiles/TileContainer";
 import PersonTile from "../../../common/tiles/PersonTile";
 import Header from "../../../common/Header";
+import BigMovieTile from "../../../common/tiles/BigMovieTile";
 
 const MoviePage = () => {
   const { id } = useParams();
@@ -38,6 +39,16 @@ const MoviePage = () => {
         <Loading />
       ) : (
           <>
+            <BigMovieTile
+              poster_path={movieData.poster_path}
+              title={movieData.title}
+              release_date={movieData.release_date}
+              vote_average={movieData.vote_average}
+              vote_count={movieData.vote_count}
+              genres={movieData.genres}
+              production_countries={movieData.production_countries}
+              overview={movieData.overview}
+            />
 
             <Header as="h2">Cast
               ({castCrewData.cast
