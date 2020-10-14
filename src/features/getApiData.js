5d40c60c8122b10e07463d7aefcd107e447e6499
Kalina Jakubowska <witillie@gmail.com>
2020-10-14 22:47:41 +0200

    moved to src folder

--- src/features/getApiData.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const getApiData = async (link) => {
-    let response;
-    await axios.get(link)
-        .then(data => response = data)
-        .catch(() => console.log("error"))
-    return response.data;
-};
