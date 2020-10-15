ff532074424beeb54a101b98a1e8dcc5bb3b2363
Kalina Jakubowska <witillie@gmail.com>
2020-10-15 16:15:02 +0200

    query added to listSlice

--- src/features/Search/index.js
+++ src/features/Search/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectQuery, setQuery } from "../listSlice";
 import { InputBox, Input, StyledInputIcon } from "./styled";
 
 const Search = () => {
+    const query = useSelector(selectQuery);
+    const dispatch = useDispatch();
 
     return (
         <InputBox>
-            <Input placeholder="Search for movies..." />
+            <Input
+                placeholder="Filtruj zadania"
+                value={query}
+                onChange={({ target }) => dispatch(setQuery(target.value))}
+            />
             <StyledInputIcon />
         </InputBox>
     );
--- src/features/listSlice.js
+++ src/features/listSlice.js
@@ -9,6 +9,7 @@ const listSlice = createSlice({
         activePage: 1,
         numberPages: undefined,
         activePath: "/movie/popular",
+        query: "",
     },
     reducers: {
         fetchListSuccess: (state, { payload: list }) => {
@@ -32,7 +33,10 @@ const listSlice = createSlice({
             state.activePath = "";
             state.numberPages = undefined;
             state.loading = true;
-        }
+        },
+        setQuery: (state, { payload: query }) => {
+            state.query = query;
+        },
     },
 });
 
@@ -42,10 +46,12 @@ export const {
     setActivePage,
     setActivePath,
     resetState,
+    setQuery,
 } = listSlice.actions;
 export const selectList = state => state.list.list;
 export const selectLoading = state => state.list.loading;
 export const selectActivePage = state => state.list.activePage;
 export const selectNumberPages = state => state.list.numberPages;
+export const selectQuery = state => state.list.query;
 
 export default listSlice.reducer;
