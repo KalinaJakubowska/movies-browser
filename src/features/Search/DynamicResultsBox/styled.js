97b1caa5590112d0638e257401dab361600411fb
Kalina Jakubowska <witillie@gmail.com>
2020-10-17 01:42:37 +0200

    flex added to Wrapper

--- src/features/Search/DynamicResultsBox/styled.js
+++ src/features/Search/DynamicResultsBox/styled.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
     position: absolute;
     width: 100%;
     height: 500px;
     overflow: auto;
     background-color: ${({ theme }) => theme.colors.white};
     color: ${({ theme }) => theme.colors.black};
-    padding: 16px;
     border-radius: 0 0 10px 10px;
     border: 1px solid ${({ theme }) => theme.colors.cornsilk};
     border-top: none;
