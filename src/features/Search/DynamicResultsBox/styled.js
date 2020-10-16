da5f796fc36abeaf1ab42e5f29e1eee2760d95c7
Kalina Jakubowska <witillie@gmail.com>
2020-10-16 20:47:20 +0200

    Wrapper styles added

---
+++ src/features/Search/DynamicResultsBox/styled.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 500px;
+    overflow: auto;
+    background-color: ${({ theme }) => theme.colors.white};
+    color: ${({ theme }) => theme.colors.black};
+    padding: 16px;
+    border-radius: 0 0 10px 10px;
+    border: 1px solid ${({ theme }) => theme.colors.cornsilk};
+    border-top: none;
+
+    ::-webkit-scrollbar {
+        width: 20px;
+    }
+
+    ::-webkit-scrollbar-track {
+        border-radius:10px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: ${({ theme }) => theme.colors.cornsilk};
+        border-radius: 10px;
+    }
+`;
