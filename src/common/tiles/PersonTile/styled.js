3e2fe24df440216e0fbef80727003125f4f8ca21
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2020-10-18 02:38:45 +0200

    media queries for people tile

--- src/common/tiles/PersonTile/styled.js
+++ src/common/tiles/PersonTile/styled.js
@@ -2,26 +2,60 @@ import styled from "styled-components";
 import { Wrapper, Image, DetailsContainer, Title } from "./../tileStyles";
 
 export const PersonWrapper = styled(Wrapper)`
+  width: 248px;
+  flex-direction: column;
+  padding: 16px 0;
+  align-items: center;
+
+  @media (max-width: ${({theme}) => theme.breakpoints.screenSmall}px) {
     width: 208px;
-    flex-direction: column;
-    padding: 16px;
+  }
+
+  @media (max-width: ${({theme}) => theme.breakpoints.bigMobile}px) {
+    width: 174px;
+    padding: 8px 0;
+  }
+
+  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+    width: 136px;
+  }
 `;
 
 export const PersonImage = styled(Image)`
+  width: 208px;
+  object-fit: cover;
+  object-position: top;
+  margin-bottom: 16px;
+
+  @media (max-width: ${({theme}) => theme.breakpoints.screenSmall}px) {
     width: 176px;
-    height: 231px;
-    object-fit: cover;
-    object-position: top;
-    margin-bottom: 16px;
+  }
+
+  @media (max-width: ${({theme}) => theme.breakpoints.bigMobile}px) {
+    width: 158px;
+  }
+
+  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+    width: 120px;
+  }
 `;
 
 export const PersonDetailsContainer = styled(DetailsContainer)`
-    grid-template-columns: repeat(6, 1fr);
+  grid-template-columns: repeat(6, 1fr);
+  align-items: center;
 `;
 
 export const PersonName = styled(Title)`
-    text-align: center;
-    font-size: ${({ subtitle }) => subtitle ? "18px" : "22px"};
+  text-align: center;
+  font-size: ${({subtitle}) => (subtitle ? "18px" : "22px")};
+
+  @media (max-width: ${({theme}) => theme.breakpoints.bigMobile}px) {
+    font-size: 16px;
+  }
+
+  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+    font-size: 14px;
+  }
 `;
 
 export const Subtitle = styled.p`
