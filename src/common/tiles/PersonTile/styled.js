2785a4cc73a8f79b84901f45f190429f27b8a21e
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2020-10-18 02:41:40 +0200

    margin fix

--- src/common/tiles/PersonTile/styled.js
+++ src/common/tiles/PersonTile/styled.js
@@ -43,6 +43,7 @@ export const PersonImage = styled(Image)`
 export const PersonDetailsContainer = styled(DetailsContainer)`
   grid-template-columns: repeat(6, 1fr);
   align-items: center;
+  margin: 0 auto;
 `;
 
 export const PersonName = styled(Title)`
