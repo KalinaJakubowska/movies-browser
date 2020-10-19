6f86f2cbf6496eb40d7e2a4b30ee2442017d11d5
Kalina Jakubowska <witillie@gmail.com>
2020-10-19 14:52:27 +0200

    Ratings initially styled for BigMovieTile

--- src/common/tiles/Ratings/index.js
+++ src/common/tiles/Ratings/index.js
@@ -2,18 +2,21 @@ import React from "react";
 import { Container, Star, Rate, Votes } from "./styled";
 import star from "../../../assets/star.svg";
 
-const Ratings = ({ voteAverage, voteCount }) => {
+const Ratings = ({ vote_average, vote_count, big = false }) => {
   return (
     <>
-      { voteCount
+      { vote_count
         ? (
           <Container>
             <Star src={star} />
-            <Rate>{voteAverage}</Rate>
-            <Votes>{voteCount} votes</Votes>
+            <Rate big={big}>{vote_average}
+              <Votes big={big}> {big && "/10"}</Votes>
+              <Votes big={big}> {vote_count} votes</Votes>
+            </Rate>
+
           </Container>
         )
-        : <Votes>No votes yet</Votes>
+        : <Votes big={big}>No votes yet</Votes>
       }
     </>
   );
