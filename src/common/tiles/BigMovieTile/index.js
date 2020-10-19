cc3c04b77bc6c7a3eeda013cabea0f4b9a0e826c
Kalina Jakubowska <witillie@gmail.com>
2020-10-19 15:49:42 +0200

    Rating for BigMovieTile added

--- src/common/tiles/BigMovieTile/index.js
+++ src/common/tiles/BigMovieTile/index.js
@@ -53,8 +53,9 @@ const BigMovieTile = ({
                 </DetailsWrapper>
                 <Types genre_ids={genre_ids} />
                 <Ratings
-                    voteAverage={vote_average}
-                    voteCount={vote_count}
+                    vote_average={vote_average}
+                    vote_count={vote_count}
+                    big={true}
                 />
                 {overview &&
                     <Description>
