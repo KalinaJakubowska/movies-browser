c3b73617272f80a72f4cf6920a0771ea280f2e49
KalinaJakubowska <witillie@gmail.com>
2020-10-24 23:18:35 +0200

    GlobalStyle and Provider moved to App, setting theme depending on isNormalTheme added

--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { GlobalStyle } from "./GlobalStyle";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
 import { Provider } from "react-redux";
 import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root"),
