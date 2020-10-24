97218722b29aa6a55a734f2a01c8a58bca09adbc
KalinaJakubowska <witillie@gmail.com>
2020-10-24 23:19:04 +0200

    Footer styles moved to Footer folder

--- src/styled.js
+++ src/styled.js
@@ -144,11 +144,3 @@ export const Logo = styled.img`
     height: 17px;
   }
 `;
-
-export const Footer = styled.footer`
-  margin-top: auto;
-  background: ${({theme}) => theme.unchangeableColors.black};
-  color: ${({theme}) => theme.unchangeableColors.white};
-  height: 50px;
-  max-width: 100vw;
-`;
