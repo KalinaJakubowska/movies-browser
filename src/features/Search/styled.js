f68a1eff735d0d0f05e6baff562ef034f98f8cb4
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2020-10-26 00:08:33 +0100

    added margin for input on mobile

--- src/features/Search/styled.js
+++ src/features/Search/styled.js
@@ -2,28 +2,30 @@ import styled from "styled-components";
 import magnifier from "./../../assets/magnifier.svg";
 
 export const Input = styled.input`
-    width: 432px;
-    max-width: 90vw;
-    height: 48px;
-    border: 1px solid ${({ theme }) => theme.colors.typeBox};
-    border-radius: ${({ open, value }) => open && value ? "24px 24px 0 0" : "33px"};
-    padding-left: 64px;
-    background-color: ${({ theme }) => theme.unchangeableColors.white};
+  width: 432px;
+  max-width: 90vw;
+  height: 48px;
+  border: 1px solid ${({theme}) => theme.colors.typeBox};
+  border-radius: ${({open, value}) =>
+    open && value ? "24px 24px 0 0" : "33px"};
+  padding-left: 64px;
+  background-color: ${({theme}) => theme.unchangeableColors.white};
 
   &:focus {
     outline: none;
   }
 
-    &::placeholder {
-        font-size: 16px;
-        line-height: 150%;
-        color: ${({ theme }) => theme.colors.subtitle};
-    }
+  &::placeholder {
+    font-size: 16px;
+    line-height: 150%;
+    color: ${({theme}) => theme.colors.subtitle};
+  }
 
   @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
     min-width: 288px;
     height: 44px;
     padding-left: 40px;
+    margin: 16px auto;
   }
 `;
 
