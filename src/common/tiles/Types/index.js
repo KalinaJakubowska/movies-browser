6b0539a512b65ee5efbb3ea7784da23caf50df98
KalinaJakubowska <witillie@gmail.com>
2020-10-25 21:12:48 +0100

    replacing parameter "page" in onGenreClick added

--- src/common/tiles/Types/index.js
+++ src/common/tiles/Types/index.js
@@ -2,11 +2,23 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectGenres, selectLoading, switchGenreEnabled } from "../../commonSlice";
 import { TypesWrapper, TypeBox, BigTypeBox } from "./styled";
+import { useReplacePageParameters } from "../../../features/pageParameters";
 
 const Types = ({ genre_ids, big = false, clickable = false }) => {
     const genresList = useSelector(selectGenres);
     const loading = useSelector(selectLoading);
     const dispatch = useDispatch();
+    const replacePageParameters = useReplacePageParameters();
+
+    const onGenreClick = (id) => {
+        replacePageParameters([
+            {
+                key: "page",
+                value: 1,
+            },
+        ]);
+        dispatch(switchGenreEnabled(id))
+    };
 
     return (
         <TypesWrapper>
@@ -18,7 +30,7 @@ const Types = ({ genre_ids, big = false, clickable = false }) => {
                             ? (<BigTypeBox
                                 enabled={enabled}
                                 clickable={clickable}
-                                onClick={() => dispatch(switchGenreEnabled(id))}
+                                onClick={() => onGenreClick(id)}
                                 key={id}
                             >
                                 {name}
