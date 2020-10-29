863db4efc7d6611c67d7e39c7146354f4b7efd14
KalinaJakubowska <witillie@gmail.com>
2020-10-29 19:34:11 +0100

    sunset/sunrise api added

--- src/common/commonSaga.js
+++ src/common/commonSaga.js
@@ -10,13 +10,16 @@ import language from "./language";
 
 function* fetchCommonHandler() {
     try {
-        const data = yield call(() => getApiData(
+        const genres = yield call(() => getApiData(
             `https://api.themoviedb.org/3/genre/movie/list${apiKey}${language}`
         ));
-        yield put(fetchCommonSuccess(data));
+        const sunData = yield call(() => getApiData(
+            `https://api.sunrise-sunset.org/json?lat=52.1301600&lng=21.0203400&date=today&formatted=0`
+        ));
+        yield put(fetchCommonSuccess({ genres, sunData }));
     } catch (error) {
         yield put(fetchCommonError());
-        yield call(alert, "Coś poszło nie tak");
+        yield call(alert, error);
     }
 };
 
--- src/common/commonSlice.js
+++ src/common/commonSlice.js
@@ -8,13 +8,15 @@ const commonSlice = createSlice({
         isNormalTheme: localStorage.getItem("theme")
             ? JSON.parse(localStorage.getItem("theme"))
             : true,
+        sunData: [],
     },
     reducers: {
         fetchCommon: state => {
             state.loading = true;
         },
-        fetchCommonSuccess: (state, { payload: data }) => {
-            state.genresList = data.genres.map(genre => ({ ...genre, enabled: false }));
+        fetchCommonSuccess: (state, { payload: { genres, sunData } }) => {
+            state.genresList = genres.genres.map(genre => ({ ...genre, enabled: false }));
+            state.sunData = sunData.results;
             state.loading = false;
         },
         fetchCommonError: state => {
@@ -45,5 +47,6 @@ export const {
 export const selectGenres = state => state.common.genresList;
 export const selectLoading = state => state.common.loading;
 export const selectTheme = state => state.common.isNormalTheme;
+export const selectSunData = state => state.common.sunData;
 
 export default commonSlice.reducer;
