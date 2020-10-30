9a8720377442ec5d1ef1f92b7fd26b866864e648
KalinaJakubowska <witillie@gmail.com>
2020-10-30 13:02:55 +0100

    auto theme with sunset and sunrise times added

--- src/common/Footer/index.js
+++ src/common/Footer/index.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectTheme, selectAutoTheme, setTheme, setAutoTheme } from "../commonSlice";
+import { selectSunData, selectTheme, selectAutoTheme, setNormalTheme, setAutoTheme } from "../commonSlice";
 import { Wrapper, SwitchButton, SwitchButtonBox, SwitchAutoThemeButton, Container } from "./styled";
 
 const Footer = () => {
     const dispatch = useDispatch();
     const isNormalTheme = useSelector(selectTheme);
     const isAutoTheme = useSelector(selectAutoTheme);
+    const { sunset, sunrise } = useSelector(selectSunData);
+
+
+    const setTheme = (sunrise, sunset) => {
+        const actualDate = new Date();
+
+        if (sunrise.getTime() < actualDate.getTime() && actualDate.getTime() < sunset.getTime()) {
+            dispatch(setNormalTheme(true));
+        }
+        else {
+            dispatch(setNormalTheme(false));
+        }
+    };
+
+    useEffect(() => {
+        const sunriseTime = new Date(sunrise);
+        const sunsetTime = new Date(sunset);
+
+        if (isAutoTheme && sunset && sunrise) {
+            setTheme(sunriseTime, sunsetTime)
+
+            const intervalId = setInterval(() => {
+                setTheme(sunriseTime, sunsetTime)
+            }, 60000);
+
+            return () => {
+                clearInterval(intervalId);
+            }
+        }
+    }, [isAutoTheme, sunrise, sunset]);
 
     return (
         <>
@@ -25,7 +55,7 @@ const Footer = () => {
                 <Container disabled={isAutoTheme}>
                     Light theme
                     <SwitchButtonBox
-                        onClick={() => !isAutoTheme && dispatch(setTheme(!isNormalTheme))}
+                        onClick={() => !isAutoTheme && dispatch(setNormalTheme(!isNormalTheme))}
                     >
                         <SwitchButton
                             isAutoTheme={isAutoTheme}
