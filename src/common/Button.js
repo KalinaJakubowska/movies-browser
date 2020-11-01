a8ea5779607ab954f9988f64b5952417c29c38bf
KalinaJakubowska <witillie@gmail.com>
2020-11-01 15:50:41 +0100

    underline added on hover

--- src/common/Button.js
+++ src/common/Button.js
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-    text-decoration: underline;
     border: none;
     background: none;
     color: ${({ theme }) => theme.colors.subtitle};
     font-size: 20px;
     cursor: pointer;
+
+    &:hover {
+        text-decoration: underline;
+    }
 `;
 
 export default Button;
