a954281d4de4222b812ece8bf8302b36c0dd4a83
KalinaJakubowska <witillie@gmail.com>
2020-11-01 14:44:36 +0100

    production countries semicolons fixed, condition fixed

--- src/common/tiles/BigMovieTile/index.js
+++ src/common/tiles/BigMovieTile/index.js
@@ -17,18 +17,54 @@ import noPosterImage from "./../../../assets/noPosterImage.svg";
 import Types from "../Types";
 
 const BigMovieTile = ({
-    poster_path,
-    title,
-    release_date,
-    vote_average,
-    vote_count,
-    genre_ids,
-    production_countries,
-    overview,
+  poster_path,
+  title,
+  release_date,
+  vote_average,
+  vote_count,
+  genre_ids,
+  production_countries,
+  overview,
 }) => {
-    return (
-      <BigTileWrapper as="div">
-        <DesktopWrapper>
+
+  return (
+    <BigTileWrapper as="div">
+      <DesktopWrapper>
+        <BigMovieImage
+          src={
+            poster_path
+              ? `https://image.tmdb.org/t/p/w342${poster_path}`
+              : noPosterImage
+          }
+          alt={`Plakat filmu ${title}`}
+        />
+        <BigDetailsContainer>
+          {title && <BigTileHeader>{title}</BigTileHeader>}
+          <DetailsWrapper>
+            {production_countries && production_countries.length > 0 && (
+              <Detail>
+                <MarkedDetail>Production: </MarkedDetail>
+                {production_countries.map(country => country.name).join(", ")}
+              </Detail>
+            )}
+            {release_date && (
+              <Detail>
+                <MarkedDetail>Release date: </MarkedDetail> {release_date}
+              </Detail>
+            )}
+          </DetailsWrapper>
+          <Types big={true} genre_ids={genre_ids} />
+          <Ratings
+            vote_average={vote_average}
+            vote_count={vote_count}
+            big={true}
+          />
+          {overview && <Description>{overview}</Description>}
+        </BigDetailsContainer>
+      </DesktopWrapper>
+
+      <MobileWrapper>
+        <TopWrapper>
           <BigMovieImage
             src={
               poster_path
@@ -40,10 +76,10 @@ const BigMovieTile = ({
           <BigDetailsContainer>
             {title && <BigTileHeader>{title}</BigTileHeader>}
             <DetailsWrapper>
-              {production_countries && (
+              {production_countries && production_countries.length > 0 && (
                 <Detail>
                   <MarkedDetail>Production: </MarkedDetail>
-                  {production_countries.map((country) => `${country.name}, `)}
+                  {production_countries.map(country => country.name).join(", ")}
                 </Detail>
               )}
               {release_date && (
@@ -52,52 +88,18 @@ const BigMovieTile = ({
                 </Detail>
               )}
             </DetailsWrapper>
-            <Types big={true} genre_ids={genre_ids} />
+            <Types genre_ids={genre_ids} />
             <Ratings
               vote_average={vote_average}
               vote_count={vote_count}
               big={true}
             />
-            {overview && <Description>{overview}</Description>}
           </BigDetailsContainer>
-        </DesktopWrapper>
-        <MobileWrapper>
-          <TopWrapper>
-            <BigMovieImage
-              src={
-                poster_path
-                  ? `https://image.tmdb.org/t/p/w342${poster_path}`
-                  : noPosterImage
-              }
-              alt={`Plakat filmu ${title}`}
-            />
-            <BigDetailsContainer>
-              {title && <BigTileHeader>{title}</BigTileHeader>}
-              <DetailsWrapper>
-                {production_countries && (
-                  <Detail>
-                    <MarkedDetail>Production: </MarkedDetail>
-                    {production_countries.map((country) => `${country.name}, `)}
-                  </Detail>
-                )}
-                {release_date && (
-                  <Detail>
-                    <MarkedDetail>Release date: </MarkedDetail> {release_date}
-                  </Detail>
-                )}
-              </DetailsWrapper>
-              <Types genre_ids={genre_ids} />
-              <Ratings
-                vote_average={vote_average}
-                vote_count={vote_count}
-                big={true}
-              />
-            </BigDetailsContainer>
-          </TopWrapper>
-          {overview && <Description>{overview}</Description>}
-        </MobileWrapper>
-      </BigTileWrapper>
-    );
+        </TopWrapper>
+        {overview && <Description>{overview}</Description>}
+      </MobileWrapper>
+    </BigTileWrapper>
+  );
 };
 
 export default BigMovieTile;
