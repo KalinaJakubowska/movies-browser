d173a6b6efee3ca463486db5ec7a1443edab0128
KalinaJakubowska <witillie@gmail.com>
2020-11-12 14:20:26 +0100

    margins changed, unnecessary code removed

--- src/common/tiles/Types/styled.js
+++ src/common/tiles/Types/styled.js
@@ -3,39 +3,31 @@ import styled, { css } from "styled-components";
 export const TypesWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  margin-right: -8px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.bigMobile}px) {
-    margin-bottom: 0;
-  }
+  margin: -4px;
 `;
 
 export const TypeBox = styled.div`
   padding: 8px 16px;
   background-color: ${({ theme }) => theme.colors.typeBox};
   border-radius: 5px;
-  margin: 0 8px 8px 0;
+  margin: 4px;
   font-size: 14px;
   line-height: 100%;
   color: ${({ theme }) => theme.colors.text};
-  flex-grow: 0;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.bigMobile}px) {
     font-size: 12px;
-    font-weight: 400;
     padding: 6px 12px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
     font-size: 10px;
-    font-weight: 400;
     padding: 4px 8px;
   }
 `;
 
 export const BigTypeBox = styled(TypeBox)`
   line-height: 140%;
-  margin-right: 8px;
 
   ${({ clickable }) => clickable && css`
     cursor: pointer;
