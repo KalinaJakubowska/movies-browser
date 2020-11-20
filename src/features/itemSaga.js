6113e71c34673be9590212422b0612431f6e6936
KalinaJakubowska <witillie@gmail.com>
2020-11-20 22:22:28 +0100

    200ms delay added

--- src/features/itemSaga.js
+++ src/features/itemSaga.js
@@ -1,32 +1,31 @@
-import { takeEvery, call, put, all } from "redux-saga/effects";
+import { takeEvery, call, put, all, delay } from "redux-saga/effects";
 import { getApiData } from "../getApiData";
-import {
-    fetchItemError,
-    fetchItemSuccess,
-    fetchItem,
-} from "./itemSlice";
+import { fetchItemError, fetchItemSuccess, fetchItem } from "./itemSlice";
 import { apiBaseLink, language, apiKey } from "./../common/commonValues";
 
 function* fetchItemHandler({ payload: { id, type } }) {
-    const activeItemPath = type === "movie"
-        ? `${apiBaseLink}movie/${id}${apiKey}${language}`
-        : `${apiBaseLink}person/${id}${apiKey}${language}`
+  const activeItemPath =
+    type === "movie"
+      ? `${apiBaseLink}movie/${id}${apiKey}${language}`
+      : `${apiBaseLink}person/${id}${apiKey}${language}`;
 
-    const activeExtraPath = type === "movie"
-        ? `${apiBaseLink}movie/${id}/credits${apiKey}`
-        : `${apiBaseLink}person/${id}/movie_credits${apiKey}${language}`
+  const activeExtraPath =
+    type === "movie"
+      ? `${apiBaseLink}movie/${id}/credits${apiKey}`
+      : `${apiBaseLink}person/${id}/movie_credits${apiKey}${language}`;
 
-    try {
-        const [itemData, extraData] = yield all([
-            call(getApiData, activeItemPath),
-            call(getApiData, activeExtraPath)
-        ])
-        yield put(fetchItemSuccess({ itemData, extraData }));
-    } catch (error) {
-        yield put(fetchItemError());
-    }
-};
+  try {
+    yield delay(200);
+    const [itemData, extraData] = yield all([
+      call(getApiData, activeItemPath),
+      call(getApiData, activeExtraPath),
+    ]);
+    yield put(fetchItemSuccess({ itemData, extraData }));
+  } catch (error) {
+    yield put(fetchItemError());
+  }
+}
 
 export function* watchFetchItem() {
-    yield takeEvery(fetchItem.type, fetchItemHandler);
-};
+  yield takeEvery(fetchItem.type, fetchItemHandler);
+}
