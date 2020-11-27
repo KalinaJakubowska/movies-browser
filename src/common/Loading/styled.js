bd8113821ba5b60e21eb53e048d40ab31f4dea20
KalinaJakubowska <witillie@gmail.com>
2020-11-27 03:50:37 +0100

    smaller loading for mobile added

--- src/common/Loading/styled.js
+++ src/common/Loading/styled.js
@@ -1,28 +1,31 @@
 import styled from "styled-components";
 
 export const LoadingContainer = styled.div`
-    width: 100%;
-    height: 250px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  width: 100%;
+  height: 250px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Image = styled.img`
-    width: 91px;
-    height: 91px;
-    transform: rotate(360deg);
-    animation: spin;
-    animation-duration: 1.25s;
-    animation-iteration-count: infinite;
-    animation-timing-function: linear;
+  height: 91px;
+  transform: rotate(360deg);
+  animation: spin;
+  animation-duration: 1.25s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
 
-    @keyframes spin {
-        from {
-            transform: unset;
-        }
-        to {
-            transform: translateX(360deg);
-        }
+  @keyframes spin {
+    from {
+      transform: unset;
     }
+    to {
+      transform: translateX(360deg);
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    height: 40px;
+  }
 `;
